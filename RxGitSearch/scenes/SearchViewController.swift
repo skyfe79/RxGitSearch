e4570047f234a11adce02a6dfdfe28b3b023296d
Sungcheol Kim <skyfe79@gmail.com>
2016-02-17 17:56:06 +0900

    Update SearchViewController.swift

--- RxGitSearch/scenes/SearchViewController.swift
+++ RxGitSearch/scenes/SearchViewController.swift
@@ -118,7 +118,7 @@ extension SearchViewController {
                 if let repo = self.viewModel.repositoryAtIndex(indexPath.row) {
                     
                     let id = repo.id
-                    // We can not send the repo data via url, so that we post the repo data to the DataCenter
+                    // We can not send the repo data via url, so we should post the repo data to the DataCenter
                     DataCenter.instance.post(String(id), value: repo)
                     Route.push(self, url: "http://repository/detail/\(id)") { (vc, result) in
                         
